0cafbc1ec3a5ede9ce2387c8abc62fd955515aac
woic-ej <woic0102@naver.com>
2024-07-04 15:55:23 +0900

    style: InputText 컴포넌트 스타일링

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import './globals.css';
 import type { Metadata } from 'next';
 import { Noto_Sans_KR } from 'next/font/google';
-import './globals.css';
 
 const notoSans = Noto_Sans_KR({ subsets: ['latin'], weight: ['400'] });
 
---
+++ src/components/input/InputText.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { useState } from 'react';
+
+interface Prop {
+  value?: string;
+  hasLimit?: boolean;
+  maxLength: number;
+  placeholder: string;
+}
+
+/**
+ * InputText component
+ * @param value: input의 기본값
+ * @param hasLimit: 글자 수 제한 표시 여부
+ * @param maxLength: 최대 글자 수
+ * @param placeholder
+ */
+
+export default function InputText({ value = '', hasLimit = false, maxLength, placeholder }: Prop) {
+  const [inputValue, setInputValue] = useState<string>(value);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const input = event.target.value;
+    if (input.length <= maxLength) {
+      setInputValue(input);
+    }
+  };
+
+  return (
+    <div className="flex justify-end items-center w-[343px] h-[48px] relative">
+      <input
+        className="border border-solid border-midGrey rounded-[10px] py-[11px] pl-[16px] pr-[58px] 
+        w-full h-full font-body placeholder-textDisabled focus:outline-none"
+        type="text"
+        placeholder={placeholder}
+        value={inputValue}
+        maxLength={maxLength}
+        onChange={handleChange}
+      />
+      {hasLimit && (
+        <span className="absolute right-[8px] font-caption text-midGrey">
+          ({inputValue.length}/{maxLength})
+        </span>
+      )}
+    </div>
+  );
+}
