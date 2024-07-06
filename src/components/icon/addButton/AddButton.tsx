659fb32b291c280bf6e5efea68b812c6e6dc1684
sunny-son <thstkd92@gmail.com>
2024-07-06 20:16:25 +0900

    style : hover 아이콘 적용

--- src/components/icon/addButton/AddButton.tsx
+++ src/components/icon/addButton/AddButton.tsx
@@ -1,18 +1,19 @@
 import DefaultAddButton from '@/assets/icons/addButton/default-add.svg';
 import PressedAddButton from '@/assets/icons/addButton/pressed-add.svg';
+import style from './AddButton.module.css';
 
 interface Props {
   isActive?: boolean;
 }
 
 export default function AddButton({ isActive }: Props) {
-  return (
+  return isActive ? (
     <div className="flex cursor-pointer w-[1.8rem] h-[1.8rem]">
-      {isActive ? (
-        <PressedAddButton width="1.8rem" height="1.8rem" />
-      ) : (
-        <DefaultAddButton width="1.8rem" height="1.8rem" />
-      )}
+      <PressedAddButton width="1.8rem" height="1.8rem" />
+    </div>
+  ) : (
+    <div className="flex cursor-pointer w-[1.8rem] h-[1.8rem]">
+      <DefaultAddButton className={style.defaultIcon} width="1.8rem" height="1.8rem" />
     </div>
   );
 }
