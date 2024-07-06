76d0447b97e38a49949a75f1dea34c0eaea15369
sunny-son <thstkd92@gmail.com>
2024-07-06 14:28:59 +0900

    refactor : 디렉토리 이름 변경 popup -> button

--- src/components/common/button/PopUpButton.stories.tsx
+++ src/components/common/button/PopUpButton.stories.tsx
@@ -1,4 +1,4 @@
-import PopUpButton from '@/components/common/popup/PopUpButton';
+import PopUpButton from '@/components/common/button/PopUpButton';
 import { Meta, StoryObj } from '@storybook/react';
 
 function PopUpButtons() {
