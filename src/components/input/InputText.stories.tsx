c521a8491f1f1a9fed97214d82d95d2a60d071c7
woic-ej <woic0102@naver.com>
2024-07-08 12:20:45 +0900

    stlye: InputChat 컴포넌트 생성

--- src/components/input/InputText.stories.tsx
+++ src/components/input/InputText.stories.tsx
@@ -1,19 +1,44 @@
+import InputChat from '@/components/input/InputChat';
 import InputText from '@/components/input/InputText';
 import { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
 
-const meta: Meta<typeof InputText> = {
-  title: 'InputText',
-  component: InputText,
+function Inputs() {
+  const [inputValue, setInputValue] = useState<string>('');
+  const maxLength = 10;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const input = event.target.value;
+    if (input.length <= maxLength) {
+      setInputValue(input);
+    }
+  };
+
+  return (
+    <div className="grid grid-cols-2">
+      <div className="flex flex-col gap-12">
+        <h1 className="text-20">Input Text</h1>
+        <InputText value="Input" hasLimit={false} maxLength={maxLength} placeholder="txt_guide" />
+        <InputText value="Input" hasLimit maxLength={maxLength} placeholder="txt_guide" />
+        <InputText value="" hasLimit maxLength={maxLength} placeholder="txt_guide" />
+      </div>
+      <div className="flex flex-col gap-12">
+        <h1 className="text-20">Input Chat</h1>
+        <InputChat value={inputValue} maxLength={maxLength} placeholder="이야기를 적어주세요" onChange={handleChange} />
+      </div>
+    </div>
+  );
+}
+
+const meta: Meta<typeof Inputs> = {
+  title: 'Inputs',
+  component: Inputs,
 };
 
 export default meta;
 
-type Story = StoryObj<typeof InputText>;
+type Story = StoryObj<typeof Inputs>;
 
 export const Default: Story = {
-  args: {
-    value: '',
-    hasLimit: true,
-    maxLength: 10,
-  },
+  args: {},
 };
