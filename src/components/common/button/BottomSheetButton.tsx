6726f237dc564c919babf0c62b0f1e93b03927b3
sunny-son <thstkd92@gmail.com>
2024-07-08 20:39:14 +0900

    refactor : AgreeButton -> BottomSheetButton 으로 변경

--- src/components/common/button/BottomSheetButton.tsx
+++ src/components/common/button/BottomSheetButton.tsx
@@ -1,10 +1,14 @@
-export default function AgreeButton() {
+interface Props {
+  text: string;
+}
+
+export default function BottomSheetButton({ text }: Props) {
   return (
     <button
       type="button"
       className="flex w-[34.3rem] h-[4.8rem] py-[1.1rem] px-[15.3rem] justify-center items-center rounded-2xl bg-primary text-18 font-500 text-white"
     >
-      동의
+      {text}
     </button>
   );
 }
