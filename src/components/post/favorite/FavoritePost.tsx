37492af93ce8e8495ae15fe8c4a4197da5f5f447
sunny-son <thstkd92@gmail.com>
2024-07-11 17:59:59 +0900

    style : heartIcon / 밑줄 스타일 추가

--- src/components/post/favorite/FavoritePost.tsx
+++ src/components/post/favorite/FavoritePost.tsx
@@ -1,3 +1,4 @@
+import HeartIcon from '@/assets/icons/bottomBar/activeHeart.svg';
 import defaultImage from '@/assets/icons/logo/logo_woory.svg';
 import DefaultPostImage from '@/components/post/DefaultPostImage';
 import { getTime } from '@/utils/getTime';
@@ -12,7 +13,7 @@ interface Props {
 export default function FavoritePost({ content, imgUrl, postDate }: Props) {
   const date = getTime(postDate);
   return (
-    <div className="relative w-[37.5rem] h-[9.3rem] bg-white border-[1px]">
+    <div className="relative w-[37.5rem] h-[9.3rem] bg-white">
       <div className="flex absolute top-[1.6rem] left-[1.6rem] w-[6rem] h-[6rem] justify-center items-center rounded-2xl">
         {imgUrl ? (
           <Image
@@ -30,6 +31,8 @@ export default function FavoritePost({ content, imgUrl, postDate }: Props) {
         {content}
       </div>
       <div className="absolute bottom-[2.2rem] left-[9.2rem] text-textDisabled text-14 font-500">{date}</div>
+      <HeartIcon width="2rem" height="1.8rem" color="#FF95B9" className="absolute top-[3.7rem] right-[1.8rem]" />
+      <div className="absolute left-[2.4rem] bottom-0 bg-lightGrey flex w-[32.7rem] h-[0.1rem] justify-center items-center flex-shrink-0" />
     </div>
   );
 }
