ad499dbb344f57fec4e1bc4e3306e595e6a348b4
sunny-son <thstkd92@gmail.com>
2024-07-14 20:56:24 +0900

    feat : zustand 적용하여 checkbox + 버튼 활성화 추가

--- src/app/(beforeLogin)/_components/Terms.tsx
+++ src/app/(beforeLogin)/_components/Terms.tsx
@@ -1,12 +1,22 @@
 'use client';
 
 import Checkbox from '@/app/_components/checkbox/Checkbox';
+import { useTermsStore } from '@/app/_store/termsStore';
+import { useState } from 'react';
 
 export default function Terms() {
+  const [isChecked, setIsChecked] = useState(false);
+  const { isAgreed, setIsAgreed } = useTermsStore();
+
+  const handleClick = () => {
+    setIsChecked((prev) => !prev);
+    setIsAgreed(!isAgreed);
+  };
+
   return (
     <div className="flex items-start gap-8 w-[34rem]">
       <div className="pt-[0.5rem]">
-        <Checkbox />
+        <Checkbox isChecked={isChecked} onClick={handleClick} />
       </div>
       <span className="w-[29.9rem] h-[5.2rem] text-18 font-500 text-midGrey">
         우리 <span className="underline underline-offset-2">이용약관,</span>{' '}
--- src/app/_store/termsStore.ts
+++ src/app/_store/termsStore.ts
@@ -2,17 +2,10 @@ import { create } from 'zustand';
 
 interface TermsState {
   isAgreed: boolean;
-  setIsAgreed(isAgreed: boolean): void;
-  reset(): void;
+  setIsAgreed: (isAgreed: boolean) => void;
 }
 
 export const useTermsStore = create<TermsState>((set) => ({
   isAgreed: false,
-  setIsAgreed(isAgreed) {
-    set({ isAgreed });
-  },
-
-  reset() {
-    set({ isAgreed: false });
-  },
+  setIsAgreed: (isAgreed: boolean) => set((state) => ({ ...state, isAgreed })),
 }));
