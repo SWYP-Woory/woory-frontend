b05ba7ee1fd08f81d2b69039d24b17b36a41f892
sunny-son <thstkd92@gmail.com>
2024-07-14 16:12:23 +0900

    style : SocialLoginButton 컴포넌트 수정

---
+++ src/components/common/button/SocialButton.stories.tsx
@@ -0,0 +1,24 @@
+import SocialLoginButton from '@/components/common/button/SocialLoginButton';
+import { Meta, StoryObj } from '@storybook/react';
+
+function SocialLoginButtons() {
+  return (
+    <div className="flex flex-col gap-8">
+      <SocialLoginButton socialDomain="kakao" />
+      <SocialLoginButton socialDomain="kakao" isActive />
+      <SocialLoginButton socialDomain="naver" />
+      <SocialLoginButton socialDomain="naver" isActive />
+    </div>
+  );
+}
+
+const meta: Meta<typeof SocialLoginButtons> = {
+  title: 'SocialLoginButton',
+  component: SocialLoginButtons,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof SocialLoginButtons>;
+
+export const Default: Story = {};
