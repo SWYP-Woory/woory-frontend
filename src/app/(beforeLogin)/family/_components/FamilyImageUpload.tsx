e8b16afb85a3aeab848e21781e382d27f5c84424
sunny-son <thstkd92@gmail.com>
2024-07-16 22:20:43 +0900

    refactor : 커스텀 훅으로 중복코드 추출

--- src/app/(beforeLogin)/family/_components/FamilyImageUpload.tsx
+++ src/app/(beforeLogin)/family/_components/FamilyImageUpload.tsx
@@ -2,26 +2,10 @@
 
 import PictureChangeButton from '@/app/_components/common/button/PictureChangeButton';
 import FamilyProfile from '@/app/_components/common/profile/FamilyProfile';
-import { ChangeEvent, useRef, useState } from 'react';
+import { useImageUpload } from '@/app/_hooks/useImageUpload';
 
 export default function FamilyImageUpload() {
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
-  const [selectedImage, setSelectedImage] = useState<string | null>(null);
-
-  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setSelectedImage(reader.result as string);
-      };
-      reader.readAsDataURL(file);
-    }
-  };
-
-  const handleUploadClick = () => {
-    fileInputRef.current?.click();
-  };
+  const { selectedImage, handleUploadClick, fileInputRef, handleImageChange } = useImageUpload();
 
   return (
     <>
--- src/app/_components/imageUpload/ImageUpload.tsx
+++ src/app/_components/imageUpload/ImageUpload.tsx
@@ -1,32 +1,12 @@
 'use client';
 
 import ImageDeleteIcon from '@/app/_components/icon/delete/ImageDeleteIcon';
+import { useImageUpload } from '@/app/_hooks/useImageUpload';
 import Picture from '@/assets/icons/picture/picture.svg';
 import Image from 'next/image';
-import { ChangeEvent, useRef, useState } from 'react';
 
 export default function ImageUpload() {
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
-  const [selectedImage, setSelectedImage] = useState<string | null>(null);
-
-  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setSelectedImage(reader.result as string);
-      };
-      reader.readAsDataURL(file);
-    }
-  };
-
-  const handleUploadClick = () => {
-    fileInputRef.current?.click();
-  };
-
-  const handleDeleteClick = () => {
-    setSelectedImage(null);
-  };
+  const { selectedImage, handleUploadClick, fileInputRef, handleImageChange, handleDeleteClick } = useImageUpload();
 
   if (selectedImage) {
     return (
