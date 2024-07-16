4e2d2f201ec3a1fb15b85be2ca26cc7d3aa6da4f
sunny-son <thstkd92@gmail.com>
2024-07-16 22:36:00 +0900

    feat : text store reset 함수 추가

--- src/app/_store/inputStore.ts
+++ src/app/_store/inputStore.ts
@@ -3,9 +3,11 @@ import { create } from 'zustand';
 interface InputState {
   inputText: string;
   setInputText: (text: string) => void;
+  reset: () => void;
 }
 
 export const useInputStore = create<InputState>((set) => ({
   inputText: '',
   setInputText: (text) => set({ inputText: text }),
+  reset: () => set({ inputText: '' }),
 }));
