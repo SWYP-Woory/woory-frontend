6114e487d0c416856351b78e33cef49351582846
woic-ej <woic0102@naver.com>
2024-07-16 17:31:10 +0900

    chore: layout.tsx 위치 이동

--- src/app/(afterLogin)/favorites/page.tsx
+++ src/app/(afterLogin)/favorites/page.tsx
@@ -9,96 +9,16 @@ const DUMMY_DATA = [
     imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
     postDate: new Date(),
   },
-  {
-    content: '오늘의 기분은?',
-    imgUrl: '',
-    postDate: new Date(),
-  },
-  {
-    content: '먹고싶은 음식 말하기',
-    imgUrl: '',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘 먹은 점심자랑하기',
-    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘의 기분은?',
-    imgUrl: '',
-    postDate: new Date(),
-  },
-  {
-    content: '먹고싶은 음식 말하기',
-    imgUrl: '',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘 먹은 점심자랑하기',
-    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘의 기분은?',
-    imgUrl: '',
-    postDate: new Date(),
-  },
-  {
-    content: '먹고싶은 음식 말하기',
-    imgUrl: '',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘 먹은 점심자랑하기',
-    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘의 기분은?',
-    imgUrl: '',
-    postDate: new Date(),
-  },
-  {
-    content: '먹고싶은 음식 말하기',
-    imgUrl: '',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘 먹은 점심자랑하기',
-    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘의 기분은?',
-    imgUrl: '',
-    postDate: new Date(),
-  },
-  {
-    content: '먹고싶은 음식 말하기',
-    imgUrl: '',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘 먹은 점심자랑하기',
-    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘의 기분은?',
-    imgUrl: '',
-    postDate: new Date(),
-  },
 ];
 
 export default function FavoritesPage() {
   return (
-    <div className="flex flex-col min-h-screen relative bg-white shadow-borderShadow">
+    <>
       <NotificationHeader title={HEDER_TITLE} />
       <div className="flex flex-col flex-grow py-[0.7rem]">
         <FavoritePostView data={DUMMY_DATA} />
       </div>
       <BottomMenuBar />
-    </div>
+    </>
   );
 }
