3349a0c8fb7a4c1324f9aee12b0d560272941042
woic-ej <woic0102@naver.com>
2024-07-16 17:23:18 +0900

    style: RavoritePostView 컴포넌트 추가

--- src/app/(afterLogin)/favorites/page.tsx
+++ src/app/(afterLogin)/favorites/page.tsx
@@ -1,4 +1,4 @@
-import FavoritePost from '@/app/(afterLogin)/favorites/_components/FavoritePost';
+import FavoritePostView from '@/app/(afterLogin)/favorites/_components/FavoritePostView';
 import BottomMenuBar from '@/app/_components/common/bottomMenuBar/BottomMenuBar';
 import NotificationHeader from '@/app/_components/common/header/NotificationHeader';
 
@@ -19,16 +19,84 @@ const DUMMY_DATA = [
     imgUrl: '',
     postDate: new Date(),
   },
+  {
+    content: '오늘 먹은 점심자랑하기',
+    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘의 기분은?',
+    imgUrl: '',
+    postDate: new Date(),
+  },
+  {
+    content: '먹고싶은 음식 말하기',
+    imgUrl: '',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘 먹은 점심자랑하기',
+    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘의 기분은?',
+    imgUrl: '',
+    postDate: new Date(),
+  },
+  {
+    content: '먹고싶은 음식 말하기',
+    imgUrl: '',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘 먹은 점심자랑하기',
+    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘의 기분은?',
+    imgUrl: '',
+    postDate: new Date(),
+  },
+  {
+    content: '먹고싶은 음식 말하기',
+    imgUrl: '',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘 먹은 점심자랑하기',
+    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘의 기분은?',
+    imgUrl: '',
+    postDate: new Date(),
+  },
+  {
+    content: '먹고싶은 음식 말하기',
+    imgUrl: '',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘 먹은 점심자랑하기',
+    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘의 기분은?',
+    imgUrl: '',
+    postDate: new Date(),
+  },
 ];
 
 export default function FavoritesPage() {
   return (
-    <div className="min-h-screen relative bg-white shadow-borderShadow">
+    <div className="flex flex-col min-h-screen relative bg-white shadow-borderShadow">
       <NotificationHeader title={HEDER_TITLE} />
-      <div className="py-[0.7rem]">
-        {DUMMY_DATA.map(({ content, imgUrl, postDate }) => (
-          <FavoritePost content={content} imgUrl={imgUrl} postDate={postDate} />
-        ))}
+      <div className="flex flex-col flex-grow py-[0.7rem]">
+        <FavoritePostView data={DUMMY_DATA} />
       </div>
       <BottomMenuBar />
     </div>
