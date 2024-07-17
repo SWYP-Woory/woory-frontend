fa05536eb4f062faf272bdf7e4a9d9f945f9535f
sunny-son <thstkd92@gmail.com>
2024-07-17 21:42:17 +0900

    style : 글 없을 경우 레이아웃 스타일 추가

--- src/app/(afterLogin)/(main)/daily/[day]/_components/daily/DailyThread.tsx
+++ src/app/(afterLogin)/(main)/daily/[day]/_components/daily/DailyThread.tsx
@@ -6,12 +6,20 @@ import GreyHeartIcon from '@/app/_components/icon/reaction/GreyHeartIcon';
 import { DailyThreadType } from '@/type';
 
 interface Props {
-  data: DailyThreadType;
+  data?: DailyThreadType;
 }
 
 const DUMMY_TARGET_TYPE = 'post';
 
 export default function DailyThread({ data }: Props) {
+  if (!data) {
+    return (
+      <div className="flex flex-col flex-grow justify-center items-center text-center w-full min-h-[15.0rem] font-body text-textDisabled">
+        아직 게시글이 없어요. <br />
+        가장 먼저 글을 남겨보세요.
+      </div>
+    );
+  }
   const { profileUrl, name, comment, reaction, content, postUrl, isEdit } = data;
   return (
     <div className="flex gap-8 w-[37.5rem] min-h-[12.1rem] pl-16 pt-16 pb-[1.4rem] pr-[2.6rem] bg-white border-b border-bgGrey">
