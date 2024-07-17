ff5ee6816240faea999c39389e30e00591db5a52
sunny-son <thstkd92@gmail.com>
2024-07-17 21:41:13 +0900

    feat : DailyView Props 에서 state 자체로 관리하도록 변경

--- src/app/(afterLogin)/(main)/daily/[day]/_components/daily/DailyView.tsx
+++ src/app/(afterLogin)/(main)/daily/[day]/_components/daily/DailyView.tsx
@@ -1,19 +1,32 @@
 'use client';
 
+import DailyThread from '@/app/(afterLogin)/(main)/daily/[day]/_components/daily/DailyThread';
 import DailyTopic from '@/app/_components/common/daily/DailyTopic';
 import DateController from '@/app/_components/common/dateController/DateController';
-import DailyThread from '@/app/_components/daily/DailyThread';
 import { useDateControl } from '@/app/_hooks/useDateControl';
 import { DailyThreadType } from '@/type';
 import { format } from 'date-fns';
+import { useState } from 'react';
 
-interface Props {
-  topic: string;
-  data: DailyThreadType;
-}
+const DUMMY_TOPIC = '내일 지구가 멸망한다면';
+const PROFILE_IMAGE =
+  'https://avatars.githubusercontent.com/u/49144662?s=400&u=903e697529c3b51f9c69bc3885c8f9be3d754028&v=4';
+const POST_URL = 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg';
 
-export default function DailyView({ topic, data }: Props) {
+const DUMMY_DATA = {
+  profileUrl: PROFILE_IMAGE,
+  name: '아빠',
+  content: '가족들과 함께 있기',
+  comment: 1,
+  reaction: 2,
+  postUrl: POST_URL,
+  isEdit: true,
+};
+
+export default function DailyView() {
   const { currentDate, prevDayHandler, nextDayHandler } = useDateControl();
+  const [topic] = useState<string>(DUMMY_TOPIC);
+  const [data] = useState<DailyThreadType>(DUMMY_DATA);
 
   return (
     <div className="flex flex-col items-center gap-24">
--- src/app/(afterLogin)/(main)/daily/[day]/page.tsx
+++ src/app/(afterLogin)/(main)/daily/[day]/page.tsx
@@ -1,3 +1,4 @@
+import DailyView from '@/app/(afterLogin)/(main)/daily/[day]/_components/daily/DailyView';
 import UpperTabBar from '@/app/_components/upperTabBar/UpperTabBar';
 
 export default function DailyPage() {
@@ -6,7 +7,9 @@ export default function DailyPage() {
       <div className="mt-8 mx-16">
         <UpperTabBar />
       </div>
-      <div />
+      <div className="mt-24">
+        <DailyView />
+      </div>
     </>
   );
 }
