cc02021d8719f1a2ad1087b140ea431e60325ab9
sunny-son <thstkd92@gmail.com>
2024-07-18 16:00:39 +0900

    style : Calendar 페이지 스타일 추가

---
+++ src/app/(afterLogin)/(main)/home/calendar/[month]/page.tsx
@@ -0,0 +1,15 @@
+import CalendarView from '@/app/(afterLogin)/(main)/home/calendar/[month]/_components/calendar/CalendarView';
+import UpperTabBar from '@/app/_components/upperTabBar/UpperTabBar';
+
+export default function CalendarPage() {
+  return (
+    <>
+      <div className="mt-8 mx-16">
+        <UpperTabBar />
+      </div>
+      <div className="flex flex-col items-center mt-24">
+        <CalendarView />
+      </div>
+    </>
+  );
+}
