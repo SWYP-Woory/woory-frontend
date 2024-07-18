dd5c1bf3cfcad13f8003b5e5885f430fa0a89488
sunny-son <thstkd92@gmail.com>
2024-07-18 16:41:31 +0900

    style : Tab Link 컴포넌트 추가

--- src/app/_components/upperTabBar/UpperTabBar.tsx
+++ src/app/_components/upperTabBar/UpperTabBar.tsx
@@ -2,18 +2,28 @@
 
 import TabButton from '@/app/_components/upperTabBar/TabButton';
 import { TabType } from '@/type';
+import { getCalendarTime } from '@/utils/getTime';
+import { format } from 'date-fns';
+import Link from 'next/link';
 import { useState } from 'react';
 
 export default function UpperTabBar() {
   const [clickTab, setClickTab] = useState<TabType>('daily');
+  const date = getCalendarTime(new Date());
+  const calendar = format(date, 'yyyy-MM');
+
   const handleClick = (value: TabType) => {
     setClickTab(value);
   };
 
   return (
     <div className="flex justify-center items-center w-[34.3rem] h-[3.2rem] rounded-2xl bg-lightGrey">
-      <TabButton tabType="daily" isClicked={clickTab === 'daily'} onClick={handleClick} />
-      <TabButton tabType="calendar" isClicked={clickTab === 'calendar'} onClick={handleClick} />
+      <Link href={`/home/daily/${date}`}>
+        <TabButton tabType="daily" isClicked={clickTab === 'daily'} onClick={handleClick} />
+      </Link>
+      <Link href={`/home/calendar/${calendar}`}>
+        <TabButton tabType="calendar" isClicked={clickTab === 'calendar'} onClick={handleClick} />
+      </Link>
     </div>
   );
 }
