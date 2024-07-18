faca394a26fb8159b5bdf624816bfb7a5049a5a8
sunny-son <thstkd92@gmail.com>
2024-07-18 16:59:48 +0900

    fix : zustand reset 함수 추가 및 BottomMenuBar 적용

--- src/app/_components/common/bottomMenuBar/BottomMenuBar.tsx
+++ src/app/_components/common/bottomMenuBar/BottomMenuBar.tsx
@@ -4,18 +4,24 @@ import HeartIcon from '@/app/_components/icon/bottomBar/HeartIcon';
 import HomeIcon from '@/app/_components/icon/bottomBar/HomeIcon';
 import MemberIcon from '@/app/_components/icon/bottomBar/MemberIcon';
 import MyPageIcon from '@/app/_components/icon/bottomBar/MyPageIcon';
+import { useHomeTabStore } from '@/app/_store/homeTabStore';
 import { getCalendarTime } from '@/utils/getTime';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 export default function BottomMenuBar() {
+  const { reset } = useHomeTabStore();
   const pathName = usePathname();
   const date = getCalendarTime(new Date());
+
+  const handleHomeTabClick = () => {
+    reset();
+  };
 
   return (
     <nav className="sticky bottom-0">
       <div className="flex justify-evenly items-start pt-[1.3rem] bg-white w-[37.5rem] h-[4.8rem] border-bgGrey border-t">
-        <Link href={`/home/daily/${date}`}>
+        <Link href={`/home/daily/${date}`} onClick={handleHomeTabClick}>
           <HomeIcon isActive={pathName.includes('home')} />
         </Link>
         <Link href="/favorites">
