95cc26014a7727b5ae601232807c63fc16ad5f2c
sunny-son <thstkd92@gmail.com>
2024-07-19 15:16:24 +0900

    feat : oauth kakao, naver 링크 연결

---
+++ src/app/_constants/domain.ts
@@ -0,0 +1,5 @@
+export const DOMAIN = 'https://woory-api.duckdns.org';
+
+export const CLIENT_KAKAO = `${DOMAIN}/oauth2/authorization/kakao`;
+
+export const CLIENT_NAVER = `${DOMAIN}/oauth2/authorization/naver`;
