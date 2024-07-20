ce5982bb42b39680a9fd8beda27f147a31e6793f
woic-ej <woic0102@naver.com>
2024-07-20 15:29:58 +0900

    chore: 사진 모달 페이지로 분리 & 마지막 답 댓글 수정,삭제 버튼 위치 변경

--- src/app/(afterLogin)/posts/[postId]/image-modal/page.tsx
+++ src/app/(afterLogin)/posts/[postId]/image-modal/page.tsx
@@ -13,21 +13,19 @@ export default function ImageModal() {
     router.back();
   };
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-20">
-      <div className="flex relative min-h-screen justify-center items-center bg-black">
-        <button type="button" aria-label="image close" className="absolute top-[1.4rem] left-16" onClick={handleClose}>
-          <ImageCloseIcon />
-        </button>
-        <div className="relative w-full">
-          <Image
-            src={postUrl!}
-            alt="post"
-            layout="intrinsic"
-            width={343}
-            height={500}
-            className="object-contain w-full h-auto"
-          />
-        </div>
+    <div className="flex relative min-h-screen justify-center items-center bg-black">
+      <button type="button" aria-label="image close" className="absolute top-[1.4rem] left-16" onClick={handleClose}>
+        <ImageCloseIcon />
+      </button>
+      <div className="relative w-full">
+        <Image
+          src={postUrl!}
+          alt="post"
+          layout="intrinsic"
+          width={343}
+          height={500}
+          className="object-contain w-full h-auto"
+        />
       </div>
     </div>
   );
