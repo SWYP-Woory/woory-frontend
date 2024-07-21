a90ae98fe55cfc5796449893a9a15072f1075102
sunny-son <thstkd92@gmail.com>
2024-07-21 20:48:50 +0900

    fix : 테스트로 해당 코드 주석처리

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,15 +1,12 @@
 import Main from '@/app/(beforeLogin)/_components/Main';
-import { getCalendarTime } from '@/utils/getTime';
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const cookieStore = cookies();
-  const accessToken = cookieStore.get('AccessToken');
+  // const cookieStore = cookies();
+  // const accessToken = cookieStore.get('AccessToken');
 
-  if (accessToken) {
-    const today = getCalendarTime(new Date());
-    redirect(`/home/daily/${today}`);
-  }
+  // if (accessToken) {
+  //   const today = getCalendarTime(new Date());
+  //   redirect(`/home/daily/${today}`);
+  // }
   return <Main />;
 }
