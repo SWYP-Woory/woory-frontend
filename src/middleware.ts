8d03ed405f5963cc893c40247134c1911cc60154
sunny-son <thstkd92@gmail.com>
2024-07-21 21:00:43 +0900

    fix : 코드 원복

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,25 @@
-// import { cookies } from 'next/headers';
-// import { NextRequest, NextResponse } from 'next/server';
-//
-// export function middleware(request: NextRequest) {
-//   // const cookieStore = cookies();
-//   // const accessToken = cookieStore.get('AccessToken');
-//   // if (!accessToken) {
-//   //   return NextResponse.rewrite(new URL('/', request.url));
-//   // }
-//   //
-//   // return NextResponse.next();
-// }
-//
-// export const config = {
-//   matcher: [
-//     '/family',
-//     '/family-select',
-//     '/notification',
-//     '/home/:path*',
-//     '/posts/:path*',
-//     '/favorites',
-//     '/members',
-//     '/mypage/:path*',
-//   ],
-// };
+import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get('AccessToken');
+  if (!accessToken) {
+    return NextResponse.rewrite(new URL('/', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/family',
+    '/family-select',
+    '/notification',
+    '/home/:path*',
+    '/posts/:path*',
+    '/favorites',
+    '/members',
+    '/mypage/:path*',
+  ],
+};
