55a990de957fa90e66554c47650a7da25591c1d5
sunny-son <thstkd92@gmail.com>
2024-07-23 20:50:56 +0900

    refactor : 더미데이터 이동

--- src/app/(afterLogin)/(main)/favorites/_components/FavoritePostView.tsx
+++ src/app/(afterLogin)/(main)/favorites/_components/FavoritePostView.tsx
@@ -1,11 +1,66 @@
 import FavoritePost from '@/app/(afterLogin)/(main)/favorites/_components/FavoritePost';
 import { FavoritePostType } from '@/type';
 
-interface Props {
-  data: FavoritePostType[];
-}
+const DUMMY_DATA = [
+  {
+    content: '오늘 먹은 점심자랑하기',
+    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202406/mn_52620_20240616152756_1.jpg',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘 먹은 점심자랑하기',
+    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘 먹은 점심자랑하기',
+    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘 먹은 점심자랑하기',
+    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘 먹은 점심자랑하기',
+    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘 먹은 점심자랑하기',
+    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘 먹은 점심자랑하기',
+    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘 먹은 점심자랑하기',
+    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘 먹은 점심자랑하기',
+    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘 먹은 점심자랑하기',
+    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
+    postDate: new Date(),
+  },
+  {
+    content: '오늘 먹은 점심자랑하기',
+    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
+    postDate: new Date(),
+  },
+];
 
-export default function FavoritePostView({ data }: Props) {
+export default function FavoritePostView() {
+  const data: FavoritePostType[] = DUMMY_DATA;
   if (data.length === 0) {
     return (
       <div className="flex flex-col justify-center items-center text-center w-full flex-grow font-body text-textDisabled">
