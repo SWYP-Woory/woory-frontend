728332ce77661fbbf39e14661d81594a5be6ac93
sunny-son <thstkd92@gmail.com>
2024-07-23 20:54:47 +0900

    feat : 쿠키로 마음함 데이터 가져오기 기능 추가

--- src/app/(afterLogin)/(main)/favorites/_components/FavoritePostView.tsx
+++ src/app/(afterLogin)/(main)/favorites/_components/FavoritePostView.tsx
@@ -1,67 +1,12 @@
+'use client';
+
 import FavoritePost from '@/app/(afterLogin)/(main)/favorites/_components/FavoritePost';
+import { getCookies } from '@/app/_store/cookie/cookies';
 import { FavoritePostType } from '@/type';
 
-const DUMMY_DATA = [
-  {
-    content: '오늘 먹은 점심자랑하기',
-    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202406/mn_52620_20240616152756_1.jpg',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘 먹은 점심자랑하기',
-    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘 먹은 점심자랑하기',
-    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘 먹은 점심자랑하기',
-    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘 먹은 점심자랑하기',
-    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘 먹은 점심자랑하기',
-    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘 먹은 점심자랑하기',
-    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘 먹은 점심자랑하기',
-    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘 먹은 점심자랑하기',
-    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘 먹은 점심자랑하기',
-    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
-    postDate: new Date(),
-  },
-  {
-    content: '오늘 먹은 점심자랑하기',
-    imgUrl: 'https://cdn.smarttoday.co.kr/news/photo/202002/img_4656_0.jpg',
-    postDate: new Date(),
-  },
-];
-
 export default function FavoritePostView() {
-  const data: FavoritePostType[] = DUMMY_DATA;
-  if (data.length === 0) {
+  const data: FavoritePostType[] = getCookies('favorites');
+  if (!data || data.length === 0) {
     return (
       <div className="flex flex-col justify-center items-center text-center w-full flex-grow font-body text-textDisabled">
         마음을 누른 글이 없어요. <br />
