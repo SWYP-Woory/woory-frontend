1a9ba5557f3a32a8edbe6ed3b21348e790946482
sunny-son <thstkd92@gmail.com>
2024-07-23 20:41:20 +0900

    feat : imageUpload zustand 초기화

--- src/app/_store/imageUploadStore.ts
+++ src/app/_store/imageUploadStore.ts
@@ -3,9 +3,11 @@ import { create } from 'zustand';
 interface ImageUploadState {
   selectedImage: string | null;
   setSelectedImage: (image: string | null) => void;
+  reset: () => void;
 }
 
 export const useImageUploadStore = create<ImageUploadState>((set) => ({
   selectedImage: null,
   setSelectedImage: (image) => set({ selectedImage: image }),
+  reset: () => set({ selectedImage: null }),
 }));
