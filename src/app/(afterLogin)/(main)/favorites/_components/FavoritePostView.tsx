1371f24292855f949074ea51fa6463fe54f1a675
sunny-son <thstkd92@gmail.com>
2024-07-23 23:29:01 +0900

    feat : Next.js 전용 LocalStorage 기능 및 FavoritePost 렌더링 기능 추가

--- src/app/(afterLogin)/(main)/favorites/_components/FavoritePostView.tsx
+++ src/app/(afterLogin)/(main)/favorites/_components/FavoritePostView.tsx
@@ -1,12 +1,19 @@
 'use client';
 
 import FavoritePost from '@/app/(afterLogin)/(main)/favorites/_components/FavoritePost';
-import { getCookies } from '@/app/_store/cookie/cookies';
+import LocalStorage from '@/app/_store/localstorage/LocalStorage';
 import { FavoritePostType } from '@/type';
+import { useEffect, useState } from 'react';
 
 export default function FavoritePostView() {
-  const data: FavoritePostType[] = getCookies('favorites');
-  if (!data || data.length === 0) {
+  const [favoritePosts, setFavoritePosts] = useState<FavoritePostType[]>([]);
+
+  useEffect(() => {
+    const data = LocalStorage.getItem('favorites') || [];
+    setFavoritePosts([...data]);
+  }, []);
+
+  if (favoritePosts.length === 0) {
     return (
       <div className="flex flex-col justify-center items-center text-center w-full flex-grow font-body text-textDisabled">
         마음을 누른 글이 없어요. <br />
@@ -16,7 +23,7 @@ export default function FavoritePostView() {
     );
   }
 
-  return data.map(({ content, imgUrl, postDate }) => (
+  return favoritePosts.map(({ content, imgUrl, postDate }) => (
     <FavoritePost content={content} imgUrl={imgUrl} postDate={postDate} />
   ));
 }
---
+++ src/app/_store/localstorage/LocalStorage.tsx
@@ -0,0 +1,20 @@
+export default class LocalStorage {
+  static setItem(key: string, value: any) {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem(key, value);
+    }
+  }
+
+  static getItem(key: string): any {
+    if (typeof window !== 'undefined') {
+      return localStorage.getItem(key);
+    }
+    return null;
+  }
+
+  static removeItem(key: string) {
+    if (typeof window !== 'undefined') {
+      localStorage.removeItem(key);
+    }
+  }
+}
