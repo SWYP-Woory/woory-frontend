f53fd484661365ca32a5c6419e27492e9db3a1ba
sunny-son <thstkd92@gmail.com>
2024-07-24 00:28:47 +0900

    feat : Topic date 추가

--- src/app/_store/topicStore.ts
+++ src/app/_store/topicStore.ts
@@ -3,15 +3,19 @@ import { create } from 'zustand';
 interface TopicState {
   topic: string;
   topicImage: string;
+  topicDate: Date | null;
   setTopic: (topic: string) => void;
   setTopicImage: (topicImage: string) => void;
+  setTopicDate: (topicDate: Date) => void;
   reset: () => void;
 }
 
-export const useTopicImageStore = create<TopicState>((set) => ({
+export const useTopicStore = create<TopicState>((set) => ({
   topic: '',
   setTopic: (topic: string) => set((state) => ({ ...state, topicImage: topic })),
   topicImage: '',
   setTopicImage: (topicImage) => set((state) => ({ ...state, topicImage })),
-  reset: () => set({ topic: '', topicImage: '' }),
+  topicDate: null,
+  setTopicDate: (topicDate) => set((state) => ({ ...state, topicDate })),
+  reset: () => set({ topic: '', topicImage: '', topicDate: null }),
 }));
