2a4dab0eea113f63ac65cf4bb3651aa22dccdf10
sunny-son <thstkd92@gmail.com>
2024-07-25 19:41:20 +0900

    feat: 해당하는 Prop에 따라 아이콘 보여주기 여부 추가

--- src/app/_components/common/dateController/DateController.tsx
+++ src/app/_components/common/dateController/DateController.tsx
@@ -1,21 +1,44 @@
 import LeftArrowIcon from '@/app/_components/icon/arrow/LeftArrowIcon';
 import RightArrowIcon from '@/app/_components/icon/arrow/RightArrowIcon';
+import { TabType } from '@/type';
 
 interface Props {
+  controllerType: TabType;
   date: string;
   prevHandler: () => void;
   nextHandler: () => void;
+  hasPrevDay?: boolean;
+  hasNextDay?: boolean;
 }
 
-export default function DateController({ date, prevHandler, nextHandler }: Props) {
+export default function DateController({
+  controllerType,
+  date,
+  prevHandler,
+  nextHandler,
+  hasPrevDay,
+  hasNextDay,
+}: Props) {
+  const arrowStyle = (condition: boolean) => {
+    if (controllerType === 'daily') {
+      if (!condition) {
+        return 'hidden';
+      }
+    }
+    return '';
+  };
+
+  console.log('hasPrevDay', hasPrevDay);
+  console.log('hasNextDay', hasNextDay);
+
   return (
     <div className="flex items-center justify-between w-[28.0rem] h-[3.2rem]">
       <button type="button" aria-label="LeftArrowButton" onClick={prevHandler}>
-        <LeftArrowIcon />
+        <LeftArrowIcon arrowStyle={arrowStyle(hasPrevDay || true)} />
       </button>
       <div className="font-title">{date}</div>
       <button type="button" aria-label="RightArrowButton" onClick={nextHandler}>
-        <RightArrowIcon />
+        <RightArrowIcon arrowStyle={arrowStyle(hasNextDay || true)} />
       </button>
     </div>
   );
