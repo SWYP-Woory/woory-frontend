b20251ddfce9342781a6a4109b638015b4c556de
sunny-son <thstkd92@gmail.com>
2024-07-25 16:28:13 +0900

    feat : zustand topicId 추가

--- src/app/_store/topicStore.ts
+++ src/app/_store/topicStore.ts
@@ -1,9 +1,11 @@
 import { create } from 'zustand';
 
 interface TopicState {
+  topicId: number | null;
   topicTitle: string;
   topicImage: string;
   topicDate: Date | null;
+  setTopicId: (topicId: number) => void;
   setTopicTitle: (topic: string) => void;
   setTopicImage: (topicImage: string) => void;
   setTopicDate: (topicDate: Date) => void;
@@ -11,11 +13,13 @@ interface TopicState {
 }
 
 export const useTopicStore = create<TopicState>((set) => ({
+  topicId: null,
+  setTopicId: (topicId: number | null) => set((state) => ({ ...state, topicId })),
   topicTitle: '',
   setTopicTitle: (topicTitle: string) => set((state) => ({ ...state, topicTitle })),
   topicImage: '',
   setTopicImage: (topicImage) => set((state) => ({ ...state, topicImage })),
   topicDate: null,
   setTopicDate: (topicDate) => set((state) => ({ ...state, topicDate })),
-  reset: () => set({ topicTitle: '', topicImage: '', topicDate: null }),
+  reset: () => set({ topicId: null, topicTitle: '', topicImage: '', topicDate: null }),
 }));
