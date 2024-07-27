b31f33a07ebb9ef28aff05d6d111a253b96dd5f1
woic-ej <woic0102@naver.com>
2024-07-27 13:09:35 +0900

    바텀시트 스타일 수정

--- src/app/_components/common/bottomSheet/HomeShortcut.tsx
+++ src/app/_components/common/bottomSheet/HomeShortcut.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Checkbox from '@/app/_components/checkbox/Checkbox';
 import PopUpButton from '@/app/_components/common/button/PopUpButton';
 import { deleteCookies, getCookies, setCookies } from '@/app/_store/cookie/cookies';
 import { useModalStore } from '@/app/_store/modalStore';
@@ -62,16 +63,23 @@ export default function HomeShortcut() {
 
   return (
     <>
-      <div className="absolute top-[5.6rem] left-[10.8rem] px-[0.8rem] py-[1.1rem]">
+      <div className="absolute top-[5.9rem] left-[10.8rem] px-[0.8rem] py-[1.1rem]">
         <Logo width="14.4rem" height="5.8rem" />
       </div>
-      <div className="absolute top-[15.2rem] left-[5.5rem] h-[6.4rem]">
+      <div className="absolute top-[14.7rem] left-[5.5rem] h-[6.4rem]">
         <h2 className="text-[2.2rem] font-700 text-black text-center">
           &apos;우리&apos; 바로가기를
           <br /> 홈 화면에 추가하시겠습니까?
         </h2>
+        <div className="flex gap-8 justify-center items-center mt-12">
+          <div className="pt-4">
+            <Checkbox />
+          </div>
+          <span className="font-body text-midGrey ">다시 보지 않기</span>
+        </div>
       </div>
-      <div className="absolute bottom-[3.2rem] left-[2rem] flex gap-[1rem]">
+
+      <div className="absolute bottom-[2.4rem] left-[2rem] flex gap-[1rem]">
         <PopUpButton text="취소" textColor="midGrey" colorType="lightGrey" size="large" onClick={handleCancelClick} />
         <PopUpButton text="추가" textColor="white" colorType="primary" size="large" onClick={handleAddClick} />
       </div>
