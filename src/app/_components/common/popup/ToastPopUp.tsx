a6b9392309269f1c02dea37ebb95e902292ac4bc
sunny-son <thstkd92@gmail.com>
2024-07-28 00:33:10 +0900

    feat : Toast 에러 메시지에 맞춰 띄우도록 수정

--- src/app/_components/common/popup/ToastPopUp.tsx
+++ src/app/_components/common/popup/ToastPopUp.tsx
@@ -3,9 +3,10 @@ import { ToastType } from '@/type';
 
 interface Props {
   type: ToastType;
+  errorMessage?: string;
 }
 
-const decideContent = (type: string) => {
+const decideContent = (type: string, errorMessage?: string) => {
   switch (type) {
     case 'link':
       return '링크가 복사되었습니다';
@@ -15,16 +16,18 @@ const decideContent = (type: string) => {
       return '사진 용량 초과 (5mb 이하 가능)';
     case 'post':
       return '하루 1개의 게시글 작성 가능';
+    case 'error':
+      return errorMessage;
     default:
       return '';
   }
 };
 
-export default function ToastPopUp({ type }: Props) {
+export default function ToastPopUp({ type, errorMessage }: Props) {
   return (
     <div className="flex justify-center items-center gap-8 bg-lightGrey w-[23.6rem] h-[4.0rem] rounded-[2.0rem]">
       <InfoIcon />
-      <div className="font-caption leading-8">{decideContent(type)}</div>
+      <div className="font-caption leading-8">{decideContent(type, errorMessage)}</div>
     </div>
   );
 }
