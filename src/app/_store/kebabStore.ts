e5e84812a5789731c7b413466392989c33a9f20c
sunny-son <thstkd92@gmail.com>
2024-07-28 15:49:25 +0900

    feat : kebabStore targetType 추가

--- src/app/_store/kebabStore.ts
+++ src/app/_store/kebabStore.ts
@@ -1,13 +1,18 @@
+import { DailyPostTitleType } from '@/type';
 import create from 'zustand';
 
 interface KebabMenuState {
   activeId: number | null;
   setActiveId: (id: number) => void;
+  targetType: DailyPostTitleType | null;
+  setTargetType: (targetType: DailyPostTitleType | null) => void;
   resetActiveId: () => void;
 }
 
 export const useKebabMenuStore = create<KebabMenuState>((set) => ({
   activeId: null,
   setActiveId: (id) => set({ activeId: id }),
-  resetActiveId: () => set({ activeId: null }),
+  targetType: null,
+  setTargetType: (targetType: DailyPostTitleType | null) => set({ targetType }),
+  resetActiveId: () => set({ activeId: null, targetType: null }),
 }));
