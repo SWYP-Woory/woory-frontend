f881116e474300a61e48eb75e0516d393868e2f1
sunny-son <thstkd92@gmail.com>
2024-08-01 16:49:48 +0900

    feat : 쿠키로 선택한 가족인지 여부 기능 추가

--- src/app/_components/common/bottomSheet/FamilySelectButton.tsx
+++ src/app/_components/common/bottomSheet/FamilySelectButton.tsx
@@ -12,9 +12,10 @@ interface Props {
 
 export default function FamilySelectButton({ familyData }: Props) {
   const router = useRouter();
+  const selectGroupId = getCookies('groupId');
 
   const handleFamilySelect = () => {
-    if (getCookies('groupId')) {
+    if (selectGroupId) {
       deleteCookies('groupId');
     }
     setCookies('groupId', familyData.groupId, { path: '/' });
@@ -22,12 +23,12 @@ export default function FamilySelectButton({ familyData }: Props) {
   };
 
   return (
-    <button type="button" onClick={handleFamilySelect}>
+    <button type="button" onClick={handleFamilySelect} className="flex items-center w-full h-full">
       <div className="flex pl-16 items-center justify-center gap-8 flex-shrink-0">
         <Profile size="small" profileImage={familyData.groupImage} isFamilyProfile />
         <h3 className="text-18 text-black font-500">{familyData.groupName}</h3>
       </div>
-      <FamilySelectIcon className="absolute top-16 right-16" />
+      {selectGroupId === familyData.groupId && <FamilySelectIcon className="absolute top-16 right-16" />}
     </button>
   );
 }
