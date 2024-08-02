be6fe20e461cf622840d406017fe5a9e50d8a41d
woic-ej <woic0102@naver.com>
2024-08-02 15:14:15 +0900

    style: arrow 크기 조정 & hover, pressed

--- src/app/_components/icon/arrow/LeftArrowIcon.tsx
+++ src/app/_components/icon/arrow/LeftArrowIcon.tsx
@@ -5,5 +5,9 @@ interface Props {
 }
 
 export default function LeftArrowIcon({ arrowStyle = '' }: Props) {
-  return <Left className={`${arrowStyle}`} />;
+  return (
+    <div className="flex justify-center items-center w-[2.4rem] h-[2.4rem] group">
+      <Left className={`${arrowStyle} fill-black group-hover:fill-midGrey group-active:fill-darkGrey`} />
+    </div>
+  );
 }
