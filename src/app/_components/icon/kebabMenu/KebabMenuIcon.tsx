376daddb1679b6f099fa0c19b8eb74f69f9bac11
sunny-son <thstkd92@gmail.com>
2024-08-03 14:26:52 +0900

    fix : <button> cannot appear as a descendant of <button> 에러 수정
    button 아래에 children이 text가 아닌 HTML이 들어갈 수 없음

--- src/app/_components/icon/kebabMenu/KebabMenuIcon.tsx
+++ src/app/_components/icon/kebabMenu/KebabMenuIcon.tsx
@@ -4,22 +4,22 @@ import KebabMenu from '@/assets/icons/kebabMenu/kebabMenu.svg';
 
 interface Props {
   isActive: boolean;
-  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  onClick: (e: React.MouseEvent<HTMLDivElement>) => void;
 }
 
 export default function KebabMenuIcon({ isActive, onClick }: Props) {
   return (
-    <button
-      type="button"
+    <div
       aria-label="kebabMenu"
       onClick={onClick}
       className="flex justify-center w-[2.4rem] h-[2.4rem] group"
+      role="presentation"
     >
       <KebabMenu
         width="0.4rem"
         height="2.0rem"
         className={`${isActive ? 'fill-primary' : 'fill-midGrey group-hover:fill-darkGrey'}`}
       />
-    </button>
+    </div>
   );
 }
