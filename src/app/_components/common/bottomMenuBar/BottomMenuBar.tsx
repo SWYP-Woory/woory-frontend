567b4d3055bc7f3bd2fb4024355d54906a12f044
woic-ej <woic0102@naver.com>
2024-08-03 15:34:42 +0900

    style: BottomMenuBar hover,pressed

--- src/app/_components/common/bottomMenuBar/BottomMenuBar.tsx
+++ src/app/_components/common/bottomMenuBar/BottomMenuBar.tsx
@@ -9,28 +9,29 @@ import { usePathname } from 'next/navigation';
 
 export default function BottomMenuBar() {
   const pathName = usePathname();
+  const isActive = (page: 'home' | 'favorites' | 'members' | 'mypage'): boolean => pathName.includes(page);
 
   return (
     <nav className="sticky bottom-0">
       <div className="flex justify-center bg-white w-[37.5rem] h-[4.8rem] border-bgGrey border-t">
         <Link href="/home/daily">
           <div className="bottom-menu">
-            <HomeIcon isActive={pathName.includes('home')} />
+            <HomeIcon isActive={isActive('home')} />
           </div>
         </Link>
         <Link href="/favorites">
           <div className="bottom-menu">
-            <HeartIcon isActive={pathName.includes('favorites')} />
+            <HeartIcon isActive={isActive('favorites')} />
           </div>
         </Link>
         <Link href="/members">
           <div className="bottom-menu">
-            <MemberIcon isActive={pathName.includes('members')} />
+            <MemberIcon isActive={isActive('members')} />
           </div>
         </Link>
         <Link href="/mypage">
           <div className="bottom-menu">
-            <MyPageIcon isActive={pathName.includes('mypage')} />
+            <MyPageIcon isActive={isActive('mypage')} />
           </div>
         </Link>
       </div>
