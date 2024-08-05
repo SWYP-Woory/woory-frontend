0e0ffeaadbd4baf2b304f883d3700abc17b8a7fb
sunny-son <thstkd92@gmail.com>
2024-08-05 20:33:33 +0900

    feat : 가족 관리 metadata 추가

--- src/app/(afterLogin)/(profile)/family-edit/layout.tsx
+++ src/app/(afterLogin)/(profile)/family-edit/layout.tsx
@@ -1,9 +1,15 @@
+import { Metadata } from 'next';
 import { ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
   modal: ReactNode;
 }
+
+export const metadata: Metadata = {
+  title: '가족 관리',
+  description: '가족 관리',
+};
 
 export default function Layout({ children, modal }: Props) {
   return (
