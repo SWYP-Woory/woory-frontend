558bde03d5456f93a2f83ee9d8d2580f7dbba61b
sunny-son <thstkd92@gmail.com>
2024-08-05 20:39:33 +0900

    feat : 알림함 metadata 추가

--- src/app/(afterLogin)/notification/page.tsx
+++ src/app/(afterLogin)/notification/page.tsx
@@ -1,5 +1,11 @@
 import NotificationList from '@/app/(afterLogin)/notification/_components/notification/NotificationList';
 import BasicHeader from '@/app/_components/common/header/BasicHeader';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '알림함',
+  description: '알림함',
+};
 
 export default function NotificationPage() {
   return (
