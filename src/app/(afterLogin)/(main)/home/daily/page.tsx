02fd374cf0fa4547030c7f477146b42f10f43c30
sunny-son <thstkd92@gmail.com>
2024-08-05 20:36:28 +0900

    feat : 데일리 metadata 추가

--- src/app/(afterLogin)/(main)/home/daily/page.tsx
+++ src/app/(afterLogin)/(main)/home/daily/page.tsx
@@ -1,4 +1,10 @@
 import DailyMain from '@/app/(afterLogin)/(main)/home/daily/_components/DailyMain';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '데일리',
+  description: '데일리',
+};
 
 export default function DailyPage() {
   return <DailyMain />;
