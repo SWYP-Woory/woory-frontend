59eb5b9f98a45d68f6b2053a53d865006e24140c
sunny-son <thstkd92@gmail.com>
2024-08-05 20:12:35 +0900

    feat : 로그인 metadata 추가

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -1,9 +1,15 @@
+import { Metadata } from 'next';
 import { ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
   modal: ReactNode;
 }
+
+export const metadata: Metadata = {
+  title: '로그인',
+  description: '로그인',
+};
 
 export default function Layout({ children, modal }: Props) {
   return (
